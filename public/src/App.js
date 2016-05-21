43a188376e55fa9d726cec28b22764c0d2c154cf
Taylor Harwood <htaylor127@gmail.com>
2016-05-21 14:05:27 -0700

    add in react

---
+++ public/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+require('./styles/app.scss');
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Hello, world.</h1>;
+    );
+  }
+}
